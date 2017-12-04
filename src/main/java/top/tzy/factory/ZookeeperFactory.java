ddcf03a54941c627516ecbdc3f06444a846514cf
zhenyutu <1572629866@qq.com>
2017-12-04 10:36:30 +0800

    完成zookeeper客户端的创建

---
+++ src/main/java/top/tzy/factory/ZookeeperFactory.java
@@ -0,0 +1,29 @@
+package top.tzy.factory;
+
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+
+/**
+ * Created by tuzhenyu on 17-12-4.
+ * @author tuzhenyu
+ */
+public class ZookeeperFactory {
+    public static  CuratorFramework client;
+
+    public static CuratorFramework create(){
+        if (client==null){
+            RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+            client = CuratorFrameworkFactory.newClient("127.0.0.1:2181", retryPolicy);
+            client.start();
+        }
+        return client;
+    }
+
+    public static void main(String[] args) throws Exception{
+        CuratorFramework client = create();
+        client.create().forPath("/netty");
+        System.out.println("finish the creation");
+    }
+}
